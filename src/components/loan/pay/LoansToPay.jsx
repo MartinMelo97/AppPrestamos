75e4efb808dcfca75d3c5839abd6f91acc5da7a7
Isaac <garciaisaac468@gmail.com>
2019-06-11 17:50:15 -0500

    first structure in pay - loan component

--- src/components/loan/pay/LoansToPay.jsx
+++ src/components/loan/pay/LoansToPay.jsx
@@ -5,14 +5,31 @@ class LoansToPay extends Component {
     constructor(props){
         super(props)
         this.state = {
-
+            customer:{
+                name: 'Alicia',
+                payNumber: 1,
+            }
         }
     }
 
     render(){
         return(
-            <div>
-                <p>LoansToPay</p>
+            <div className="pay-loan-container">
+                <p className="pay-loan-title">
+                { this.state.customer != null ?
+                customer.name
+                : 
+                null}
+                </p>
+                <span className="pay-loan-subtitle">
+                    Pago #{ this.state.customer != null ?
+                            customer.payNumber
+                            : 
+                            null}
+                </span>
+                <div className="buttons-container">
+                    <button>Pagar</button>
+                </div>
             </div>
         )
     }
