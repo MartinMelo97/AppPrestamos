77436fdf87fdc63f3aaa22400412b857e55716f1
Isaac <garciaisaac468@gmail.com>
2019-06-13 12:01:04 -0500

    finish structure and design in pay loan

--- src/components/loan/pay/LoansToPay.jsx
+++ src/components/loan/pay/LoansToPay.jsx
@@ -8,8 +8,21 @@ class LoansToPay extends Component {
             customer:{
                 name: 'Alicia',
                 payNumber: 1,
-            }
+            },
+            active: false
         }
+    }
+
+    changeActive = () =>{
+        let button = document.getElementsByClassName('button-style-two')[0]
+        let container = document.getElementsByClassName('ghost-container')[0]
+        let { active } = this.state
+
+        button.className = active ? "button-style-two not-active" : "button-style-two active"
+        container.className = active ? "ghost-container not-active" : "ghost-container active"
+        active = !active
+
+        this.setState({ active })
     }
 
     render(){
@@ -20,15 +33,24 @@ class LoansToPay extends Component {
                 this.state.customer.name
                 : 
                 null}
+                    <span className="pay-loan-subtitle">
+                        Pago #{ this.state.customer != null ?
+                                this.state.customer.payNumber
+                                : 
+                                null}
+                    </span>
                 </p>
-                <span className="pay-loan-subtitle">
-                    Pago #{ this.state.customer != null ?
-                            this.state.customer.payNumber
-                            : 
-                            null}
-                </span>
                 <div className="buttons-container">
-                    <button>Pagar</button>
+                    <button className="button-style-one">Pagar</button>
+                    <span>Ó</span>
+                    <button 
+                    className="button-style-two not-active"
+                    onClick={ () => this.changeActive() }
+                    >Parcial</button>
+                    <div className="ghost-container not-active">
+                        <input type="number" placeholder="$"/>
+                        <button className="button-add">Agregar</button>
+                    </div>
                 </div>
             </div>
         )
