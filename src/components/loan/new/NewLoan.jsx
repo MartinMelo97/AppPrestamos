6996238a3da5bfe8df150e8ce4b390096fd946d1
Isaac <garciaisaac468@gmail.com>
2019-06-15 10:23:11 -0500

    v1 of new loan

--- src/components/loan/new/NewLoan.jsx
+++ src/components/loan/new/NewLoan.jsx
@@ -14,12 +14,14 @@ class NewLoan extends Component {
                 'Pedro',
                 'Martin',
                 'Daniel',
-                'Erik',
-                'Victor'
+                'Pablo',
+                'Isaac'
             ],
             date: null,
             limit: null,
-            active: false
+            days: [],
+            active: false,
+            select: false,
         }
     }
 
@@ -39,9 +41,59 @@ class NewLoan extends Component {
         active = !active
         this.setState({ active })
     }
+    inputClient = (i) =>{
+        let select = document.getElementsByClassName(`opn${i}`)[0]
+        let space = document.getElementsByClassName(`space`)[0]
+
+        space.innerText = select.innerText
+    }
+
+    selectNumer = () =>{
+        let { select } = this.state
+        let number = document.getElementsByClassName('number-limit')[0].innerText;
+
+        select = !select
+
+        if( select ){
+            let selector = document.getElementsByClassName(`numbers-container`)[0]
+            let alloptions = document.getElementsByClassName(`number-option`)
+            let option = document.getElementById(`n${number}`)
+            for(let i = 0; i < alloptions.length; i++){
+                alloptions[i].style.color="#000"
+            }
+            try{
+                setTimeout(()=>{
+                    option.style.color="#0ac8e5"
+                    selector.scrollTo(0,40*(number-1))
+                }, 0)
+            }catch(e){
+                alert(e)
+            }
+        }
+        
+        this.setState({ select })
+    }
+    inputNumber = (day) =>{
+        let select = document.getElementById(`n${day}`)
+        let space = document.getElementsByClassName(`number-limit`)[0]
+        let options = document.getElementsByClassName(`number-option`)
+
+        for(let i = 0; i < options.length; i++){
+            options[i].style.color = "#000"
+        }
+
+        select.style.color = "#0ac8e5"
+
+        space.innerText = select.innerText
+    }
 
     componentDidMount = () =>{
         this.date()
+        let { days } = this.state
+        for(let i = 1; i < 51; i++){
+            days.push(i)
+        }
+        this.setState({ days })
     }
 
     render(){
@@ -69,10 +121,15 @@ class NewLoan extends Component {
                          className={`view-button ${this.state.active === false ? "not-active" : "active" }`} />
                         <img src={ plus } alt="agregar" className="add-button"/>
                         <div
+                        style={{
+                            overflowY : this.state.customerNames.length >= 6 ? 'scroll' : 'hidden'
+                        }}
                         className={`options-container ${this.state.active === false ? "not-active" : "active" }`}>
                             { this.state.customerNames.length > 0 ?
-                            this.state.customerNames.map((name)=>(
-                                <p className="option">{ name }</p>
+                            this.state.customerNames.map((name, i)=>(
+                                <p 
+                                className={`option opn${i}`}
+                                onClick={()=>this.inputClient(i)}>{ name }</p>
                             ))
                             :
                             null }
@@ -86,15 +143,29 @@ class NewLoan extends Component {
                     null }</span>
                     <div className="limit">
                         <span>Plazo</span>
-                        <div className="choiseDay">
-                            <span>{ this.state.limit != null ?
+                        <div className="choiseDay"
+                        onClick = {()=>this.selectNumer()} >
+                            <p 
+                            className={`number-limit ${this.state.select ? 'active' : 'not-active'}`}>
+                            { this.state.limit != null ?
                             this.state.limit
                             :
-                            null }</span>
-                            <span>Días</span>
+                            '20' }</p>
+                            <div
+                            className={`numbers-container ${this.state.select ? 'active' : 'not-active'}`} >
+                                {this.state.days.map((day)=>(
+                                    <span 
+                                    className='number-option'
+                                    id={`n${day}`}
+                                    onClick={()=>this.inputNumber(day)}>
+                                    {`${day}`}
+                                    </span>
+                                ))}
+                            </div>
                         </div>
+                        <span>Días</span>
                     </div>
-                    <button>Agregar</button>
+                    <button className="add-loan-button">Agregar</button>
                 </div>
             </div>
         )
